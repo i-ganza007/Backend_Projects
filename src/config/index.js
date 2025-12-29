35076a722bfc89014ec73e5f6564b1b8ef498046
i-ganza007 <i.ganza@alustudent.com>
2025-12-30 00:49:57 +0200

    Initial Commit

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv')
-dotenv.config()
-console.log(process.env.PORT);
-
-module.exports = {
-    PORT : process.env.PORT
-}
--- src/controllers/info-controller.js
+++
@@ -1,9 +0,0 @@
-const info = (req,res) => {
-    return res.json({
-        success:true,
-        message:'API is live',
-        error:{},
-        data:{}
-    })
-}
-module.exports = info
